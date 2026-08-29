d2a93204a7ce094f5ab0fd7c0f996aa93786f8ac
ChandruSakthi <chandrus2805@gmail.com>
2026-08-29 21:56:53 +0530

    admin board basic structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBar } from "../shared/side-bar/side-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
